63d915bb21115a4eec93d7c9ed3fa51610a44612
N3pr0Hack3r <67415662+N3pr0Hack3r@users.noreply.github.com>
2020-10-13 09:11:08 +0200

    Vytvořena třída PoklZaznam

---
+++ Pokladna/PoklZaznam.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokladna
+{
+    public class PoklZaznam
+    {
+        public int idPoklZaznam { get; set; }
+        public int cislo { get; set; }
+        public DateTime datum { get; set; }
+        public string popis { get; set; }
+        public double castka { get; set; }
+        public double zustatek { get; set; }
+        public string poznamka { get; set; }
+
+        public PoklZaznam(int idPoklZaznam, int cislo, DateTime datum, string popis, double castka, double zustatek, string poznamka)
+        {
+            this.idPoklZaznam = idPoklZaznam;
+            this.cislo = cislo;
+            this.datum = datum;
+            this.popis = popis;
+            this.castka = castka;
+            this.zustatek = zustatek;
+            this.poznamka = poznamka;
+        }
+
+        public PoklZaznam()
+        {
+        }
+    }
+}
